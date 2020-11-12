9e568f22b5e3048af1fe43c91059b6128af7eaa3
RayFirefist <rayfirefist_all@makoo.eu>
2020-11-12 18:57:38 +0100

    Added sample Live2D page

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/globals.css'
+import consts from '../consts.json';
 import ResponsiveBase from '../components/common/base';
 
 // Theme constant
 //import theme from './../utils/darkMode/darkMode';
 
 // eslint-disable-next-line
-String.prototype.format = function() {
+String.prototype.format = function () {
     let a = this;
     for (let k in arguments) {
         a = a.replace("{" + k + "}", arguments[k])
@@ -17,7 +18,7 @@ String.prototype.format = function() {
 }
 
 // eslint-disable-next-line
-Date.prototype.addDays = function(days) {
+Date.prototype.addDays = function (days) {
     var date = new Date(this.valueOf());
     date.setDate(date.getDate() + days);
     return date;
@@ -31,11 +32,20 @@ Date.prototype.addDays = function(days) {
  */
 
 class MyApp extends React.Component {
-        render() {
-            const { Component, pageProps } = this.props;
-            return <ResponsiveBase body = { < Component {...pageProps }
-                />}></ResponsiveBase >
-            }
-        }
 
-        export default MyApp
+    render() {
+        const { Component, pageProps } = this.props;
+
+        return <html>
+            <head>
+                <title>{consts.name} v{consts.version}</title>
+                <script src = "http://publicjs.supmiao.com/live2dcubismcore.min.js"></script>
+            </head>
+            <body>
+                <ResponsiveBase body={<Component {...pageProps} />}></ResponsiveBase >
+            </body>
+        </html>
+    }
+}
+
+export default MyApp
--- pages/live2d/index.jsx
+++ pages/live2d/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import Drawer from '../../components/common/base.jsx'
+import consts from '../../consts.json';
+import dynamic from 'next/dynamic'
+const ReactLive2d = dynamic(import('react-live2d'), {
+    ssr: false
+})
 
 class DjLive2dListPage extends React.Component {
+
     render() {
-        return <Drawer Body={<h1>Live2D Index Page</h1>} path={['Home', 'Live2D']}/>
+        return <>
+            <h1>Live2D Index Page</h1>
+            <div style={{backgroundImage: ("url(" + consts.cdn + "ondemand/background/bg_training.jpg)").replace("\"", "")}}>
+                <ReactLive2d
+                    width={600}
+                    height={800}
+                    //bottom={'10px'}
+                    //right={'10px'}
+                    ModelList={['Rika']}
+                    //TouchBody={['啊啊啊啊啊你要干嘛', '哼', '坏人']}
+                    PathFull={consts.cdn + "test/live2d/"}
+                    TouchDefault={null}
+                />
+            </div>
+        </>
     }
 }
 
