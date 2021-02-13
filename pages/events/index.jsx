e9ad2093dbaf3b5a08933f74dbc20dbb27713292
git <rayfirefist_all@makoo.eu>
2021-02-13 14:33:22 +0100

    Sorting events through IDs

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,63 +1,70 @@
-import React from 'react';
+import React from "react";
 
-import Link from 'next/link';
+import Link from "next/link";
 
-import { Card, Grid } from '@material-ui/core';
+import { Card, Grid } from "@material-ui/core";
 
-import consts from '../../consts.json';
-import AbstractList from '../../components/common/listPage.tsx';
-import SafeImageLoader from '../../components/common/safeImage';
+import consts from "../../consts.json";
+import AbstractList from "../../components/common/listPage.tsx";
+import SafeImageLoader from "../../components/common/safeImage";
 //import l10n from '../../utils/l10n/l10n';
 
 //const strings = new l10n();
 
 class DjEventsListPage extends AbstractList {
     constructor(props) {
-        super(props)
-        this.databases = ['EventMaster']
+        super(props);
+        this.databases = ["EventMaster"];
         this.title = "EVENTS_TITLE";
     }
 
     getIllustUrl(event) {
-        let id = event.Id.toString()
-        return consts.cdn + "ondemand/event/event_" + id + "/banner_event.png"
+        try {
+            let id = event.Id.toString();
+            return `${consts.cdn}ondemand/event/event_${id}/banner_event.png`;
+        }
+        catch (e) {
+            return null;
+        }
     }
 
     renderElements() {
-        console.log("rendering")
-        console.log(this.state);
         let events = this.state.databases.EventMaster;
         //let charas = this.state.databases.CharacterMaster;
-        console.log(events);
 
-        let out = []
+        let out = [];
 
-        for (let event in events) {
-            event = events[event]
+        Object.keys(events).sort((a, b) => (parseInt(a) > parseInt(b)) ? 1 : -1).reverse().forEach( event => {
+            event = events[`${event}`];
 
-            if (event.CardName === "※危険トランプ追加用")
-                continue;
+            if (event.CardName === "※危険トランプ追加用") return;
 
-            out.push(<Link href={"/events/" + event.Id}>
-                <Card className={this.classes.card}>
-                    <Grid container>
-                        <Grid item xs={1}></Grid>
-                        <Grid item>
-                            <SafeImageLoader src={this.getIllustUrl(event)} alt={event.Id} className={this.classes.bannerIcon} />
+            out.push(
+                <Link href={"/events/" + event.Id}>
+                    <Card className={this.classes.card}>
+                        <Grid container>
+                            <Grid item xs={1}></Grid>
+                            <Grid item>
+                                <SafeImageLoader
+                                    src={this.getIllustUrl(event)}
+                                    alt={event.Id}
+                                    className={this.classes.bannerIcon}
+                                />
+                            </Grid>
+                            <Grid item xs={1}></Grid>
+                            <Grid item>
+                                <div style={{ textAlign: "left" }}>
+                                    <b>{event.Name}</b>
+                                    <p>{event.Type._name_}</p>
+                                </div>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={1}></Grid>
-                        <Grid item>
-                            <div style={{ textAlign: "left" }}>
-                                <b>{event.Name}</b>
-                                <p>{event.Type._name_}</p>
-                            </div>
-                        </Grid>
-                    </Grid>
-                </Card>
-            </Link>)
-        }
+                    </Card>
+                </Link>
+            );
+        });
 
-        return out
+        return out;
     }
 }
 
