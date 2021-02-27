46aed63122ac49550e27969b942def83d8a0a9fd
git <rayfirefist_all@makoo.eu>
2021-02-27 11:17:06 +0100

    Gacha Assets implemented

--- pages/gacha/[id].jsx
+++ pages/gacha/[id].jsx
@@ -15,7 +15,7 @@ import timestampToString from "../../utils/time/timeString";
 import ImageLoader from "../../components/common/image";
 import SafeImageLoader from "../../components/common/safeImage";
 import D4DJCardIcon from "../../components/cards/icon";
-import consts from '../../consts.json';
+import consts from "../../consts.json";
 
 const strings = new l10n();
 
@@ -46,7 +46,15 @@ class DjGachaEntryPage extends React.Component {
 
     componentDidMount() {
         fetch("/api/dbs", {
-            body: JSON.stringify({ dbs: ["GachaMaster", "GachaTableMaster", "CardMaster", "CharacterMaster", "UnitMaster"] }),
+            body: JSON.stringify({
+                dbs: [
+                    "GachaMaster",
+                    "GachaTableMaster",
+                    "CardMaster",
+                    "CharacterMaster",
+                    "UnitMaster"
+                ]
+            }),
             headers: {
                 "Content-Type": "application/json"
             },
@@ -60,13 +68,16 @@ class DjGachaEntryPage extends React.Component {
 
                 json = json.result;
                 //const chara = json["CharacterMaster"][this.state.id];
-                //const unit = json["UnitMaster"][chara.Unit];
+                const cards = json["CardMaster"];
                 const gacha = json["GachaMaster"][this.state.id];
 
                 if (gacha === undefined)
-                    return this.setState({ error: "Chara not found", loading: false });
+                    return this.setState({
+                        error: "Chara not found",
+                        loading: false
+                    });
 
-                this.setState({ gacha: gacha, loading: false });
+                this.setState({ gacha: gacha, cards: cards, loading: false });
             })
             .catch(err => this.setState({ error: err, loading: false }));
     }
@@ -93,12 +104,33 @@ class DjGachaEntryPage extends React.Component {
     }
 
     getIllustUrl(card, illustMode = 0) {
-        return consts.cdn + "ondemand/card_icon/card_icon_0" + card.Id + "_" + illustMode + ".jpg"
+        return (
+            consts.cdn +
+            "ondemand/card_icon/card_icon_0" +
+            card.Id +
+            "_" +
+            illustMode +
+            ".jpg"
+        );
     }
 
     getIllustUrl2() {
-        let id = this.state.gacha.Id.toString()
-        return consts.cdn + "ondemand/gacha/top/banner/" + id.padStart(4, "0") + ".png"
+        let id = this.state.gacha.Id.toString();
+        return (
+            consts.cdn +
+            "ondemand/gacha/top/banner/" +
+            id.padStart(5, "0") +
+            ".png"
+        );
+    }
+
+    getIllustUrl3(_id) {
+        return (
+            consts.cdn +
+            "ondemand/gacha/top/live2d_bg/" +
+            _id +
+            ".jpg"
+        );
     }
 
     render() {
@@ -113,17 +145,36 @@ class DjGachaEntryPage extends React.Component {
 
         return (
             <div>
-                <div align="left"><h1>{gacha.Name}</h1></div>
-                <SafeImageLoader src={this.getBannerUrl()} alternativeUrl={this.getIllustUrl2()} style={{width: "100%"}} />
+                <div align="left">
+                    <h1>{gacha.Name}</h1>
+                </div>
+                <SafeImageLoader
+                    src={this.getBannerUrl()}
+                    alternativeUrl={this.getIllustUrl2()}
+                    style={{ width: "100%" }}
+                />
                 <br />
                 <br />
                 <br />
 
                 <div align="left">
                     {/* Gacha Information */}
-                    <RowInformation left={strings.getString("GACHA_NAME")} right={gacha.Name} />
-                    <RowInformation left={strings.getString("GACHA_START")} right={timestampToString(gacha.StartDate)} />
-                    <RowInformation left={strings.getString("GACHA_END")} right={timestampToString(gacha.EndDate)} />
+                    <RowInformation
+                        left={strings.getString("GACHA_NAME")}
+                        right={gacha.Name}
+                    />
+                    <RowInformation
+                        left={strings.getString("GACHA_START")}
+                        right={timestampToString(gacha.StartDate)}
+                    />
+                    <RowInformation
+                        left={strings.getString("GACHA_END")}
+                        right={timestampToString(gacha.EndDate)}
+                    />
+                    <RowInformation
+                        left={strings.getString("GACHA_TYPE")}
+                        right={gacha.Type._name_}
+                    />
 
                     {/* Gacha Rates */}
 
@@ -131,18 +182,43 @@ class DjGachaEntryPage extends React.Component {
                     <h2>{strings.getString("GACHA_CARDS_TITLE")}</h2>
                     <h3>{strings.getString("GACHA_PICKUP_CARDS_TITLE")}</h3>
                     <Grid container>
-                        {
-                            this.state.gacha.PickUpCards.map(cardId =>
+                        {this.state.gacha.PickUpCards.map(cardId => {
+                            let cardData = this.state.cards[cardId];
+                            return (
                                 <D4DJCardIcon
-                                    illustUrl={this.getIllustUrl({ Id: cardId })}
-                                    rarity={3}
-                                    typeId={1}
+                                    illustUrl={this.getIllustUrl({
+                                        Id: cardId
+                                    })}
+                                    rarity={cardData.Rarity}
+                                    typeId={cardData.Attribute}
                                 />
-                            )
-                        }
+                            );
+                        })}
                     </Grid>
 
                     {/* Assets */}
+                    <h2>{strings.getString("GACHA_ASSETS_TITLE")}</h2>
+                    <h3>{strings.getString("GACHA_BANNER_ASSETS_TITLE")}</h3>
+                    <SafeImageLoader
+                        src={this.getBannerUrl()}
+                        style={{ width: "100%" }}
+                    />
+
+                    <h3>{strings.getString("GACHA_ICON_ASSETS_TITLE")}</h3>
+                    <SafeImageLoader
+                        src={this.getIllustUrl2()}
+                        style={{ width: "100%" }}
+                    />
+
+                    <h3>{strings.getString("GACHA_LIVE2D_BG_ASSETS_TITLE")}</h3>
+                    {this.state.gacha.Live2dBg.map(file => {
+                        return (
+                            <SafeImageLoader
+                                src={this.getIllustUrl3(file)}
+                                style={{ width: "100%" }}
+                            />
+                        );
+                    })}
                 </div>
             </div>
         );
