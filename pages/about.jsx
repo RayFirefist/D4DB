8cdb90c21b6c44cab0498e92015ed15f71cb693e
git <rayfirefist_all@makoo.eu>
2021-04-14 12:39:23 +0200

    About page now uses l10n strings

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import consts from '../consts.json';
-import {GitHub, Twitter} from '@material-ui/icons';
-import { Alert } from '@material-ui/lab';
+import React from "react";
+import consts from "../consts.json";
+import { GitHub, Twitter } from "@material-ui/icons";
+import { Alert } from "@material-ui/lab";
+import l10n from "../utils/l10n/l10n";
+
+const strings = new l10n();
 
 class DjAboutPage extends React.Component {
     render() {
-        return <>
-            <h1 style={{textAlign: "left"}}>About</h1>
-            <h2>{consts.name} - {consts.version}</h2>
-            <Alert severity={"info"} style={{textAlign: "left"}}>
-                <GitHub style={{maxWidth: "20px"}}/> You can contribute to the website's code through our 
-                <a href="https://github.com/RayFirefist/D4DJ_Frontend"> GitHub repository</a> 
-            </Alert>
-            <br></br>
-            <Alert severity={"info"} style={{textAlign: "left"}}>
-                <Twitter></Twitter> Developer: <a href={"https://twitter.com/RayFirefist"}>RayFirefist</a>
-            </Alert>
-            <br></br>
-            <Alert severity={"info"} style={{textAlign: "left"}}>
-                <Twitter></Twitter> Developer: <a href={"https://twitter.com/HamP_punipuni"}>HamP_punipuni</a>
-            </Alert>
-        </>
+        return (
+            <>
+                <h1 style={{ textAlign: "left" }}>
+                    {strings.getString("ABOUT_TITLE")}
+                </h1>
+                <h2>
+                    {consts.name} - {consts.version}
+                </h2>
+                <Alert severity={"info"} style={{ textAlign: "left" }}>
+                    <GitHub style={{ maxWidth: "20px" }} />{" "}
+                    {strings.getString("ABOUT_GITHUB_1")}
+                    <a href="https://github.com/RayFirefist/D4DJ_Frontend">
+                        {" "}
+                        {strings.getString("ABOUT_GITHUB_2")}
+                    </a>{" "}
+                    {strings.getString("ABOUT_GITHUB_3")}
+                </Alert>
+                <br></br>
+                <Alert severity={"info"} style={{ textAlign: "left" }}>
+                    <Twitter></Twitter> {strings.getString("ABOUT_DEVELOPER")}:{" "}
+                    <a href={"https://twitter.com/RayFirefist"}>RayFirefist</a>
+                </Alert>
+                <br></br>
+                <Alert severity={"info"} style={{ textAlign: "left" }}>
+                    <Twitter></Twitter> {strings.getString("ABOUT_DEVELOPER")}:{" "}
+                    <a href={"https://twitter.com/HamP_punipuni"}>
+                        HamP_punipuni
+                    </a>
+                </Alert>
+            </>
+        );
     }
 }
 
