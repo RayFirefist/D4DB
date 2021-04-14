a024d6f85f12689e8a3c5b906ad71ff694cb62ea
Outvi V <git@outv.im>
2021-04-15 01:08:55 +0800

    chore(about): style tweaks

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,36 +3,58 @@ import consts from "../consts.json";
 import { GitHub, Twitter } from "@material-ui/icons";
 import { Alert } from "@material-ui/lab";
 import l10n from "../utils/l10n/l10n";
+import { withStyles } from '@material-ui/core/styles';
+
+const style = {
+    message: {
+        display: "flex",
+        alignItems: "center"
+    },
+    textLeft: {
+        textAlign: "left"
+    },
+    icon: {
+        marginRight: "6px",
+        maxWidth: "20px"
+    }
+};
 
 const strings = new l10n();
 
 class DjAboutPage extends React.Component {
     render() {
+        const { classes } = this.props;
+
         return (
             <>
-                <h1 style={{ textAlign: "left" }}>
+                <h1 className={classes.textLeft}>
                     {strings.getString("ABOUT_TITLE")}
                 </h1>
                 <h2>
                     {consts.name} - {consts.version}
                 </h2>
-                <Alert severity={"info"} style={{ textAlign: "left" }}>
-                    <GitHub style={{ maxWidth: "20px" }} />{" "}
-                    {strings.getString("ABOUT_GITHUB_1")}
+                <Alert classes={{
+                    message: classes.message
+                }} className={classes.textLeft} severity={"info"}>
+                    <GitHub className={classes.icon} />
+                    {strings.getString("ABOUT_GITHUB_1")}&nbsp;
                     <a href="https://github.com/RayFirefist/D4DJ_Frontend">
-                        {" "}
                         {strings.getString("ABOUT_GITHUB_2")}
-                    </a>{" "}
+                    </a>&nbsp;
                     {strings.getString("ABOUT_GITHUB_3")}
                 </Alert>
-                <br></br>
-                <Alert severity={"info"} style={{ textAlign: "left" }}>
-                    <Twitter></Twitter> {strings.getString("ABOUT_DEVELOPER")}:{" "}
+                <br />
+                <Alert classes={{
+                    message: classes.message
+                }} className={classes.textLeft} severity={"info"}>
+                    <Twitter className={classes.icon} /> {strings.getString("ABOUT_DEVELOPER")}:&nbsp;
                     <a href={"https://twitter.com/RayFirefist"}>RayFirefist</a>
                 </Alert>
-                <br></br>
-                <Alert severity={"info"} style={{ textAlign: "left" }}>
-                    <Twitter></Twitter> {strings.getString("ABOUT_DEVELOPER")}:{" "}
+                <br />
+                <Alert classes={{
+                    message: classes.message
+                }} className={classes.textLeft}severity={"info"}>
+                    <Twitter className={classes.icon} /> {strings.getString("ABOUT_DEVELOPER")}:&nbsp;
                     <a href={"https://twitter.com/HamP_punipuni"}>
                         HamP_punipuni
                     </a>
@@ -42,4 +64,4 @@ class DjAboutPage extends React.Component {
     }
 }
 
-export default DjAboutPage;
+export default withStyles(style)(DjAboutPage);
